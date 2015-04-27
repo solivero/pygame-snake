b243665467e671e96bd82c8565e57fddfcd0095e
Oliver Petri <oliver.ce.petri@gmail.com>
2015-04-28 00:32:13 +0200

    Grid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import random
 
-GAME_NAME = 'Snake'
+GAME_NAME = 'Gridder'
 WIDTH = 800
 HEIGHT = 600
 FPS = 30
@@ -15,6 +16,18 @@ pygame.display.set_caption(GAME_NAME)
 clock = pygame.time.Clock()
 running = True
 
+circle_size = 40
+rows = 12
+cols = 16
+
+dots = []
+# init list with rows of colors
+for i in range(rows):
+    row = []
+    for j in range(cols):
+        row.append((i*2,j*2,i*j))
+    dots.append(row)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,7 +35,16 @@ while running:
 
     game_display.fill((255,255,255))
     # display, color, x y width height
-    pygame.draw.rect(game_display, (0,0,0), [WIDTH/2-40/2,HEIGHT/2-80/2,40,80])
+    # draw circles in grid
+    current_y = 0
+
+    for row in dots:
+        current_x = 0
+        current_y += circle_size + 5
+        for dot in row:
+            current_x += circle_size + 5
+            pygame.draw.rect(game_display, dot, [current_x, current_y, circle_size, circle_size])
+
     pygame.display.update()
     clock.tick(FPS)
 
